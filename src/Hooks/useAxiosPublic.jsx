c2950467276dc6ee26b50e0699722eee4fe76b53
henryjobel <jhobelsaha@gmail.com>
2023-12-22 09:58:54 +0600

    cors error solved

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "https://task-management-server-zeta-indol.vercel.app",
+  baseURL: "https://task-management-server-sigma.vercel.app",
 });
 
 const useAxiosPublic = () => {
