7645c04600025f2a82ac125865f66aaa3853f420
henryjobel <jhobelsaha@gmail.com>
2023-12-22 06:30:36 +0600

    banner heading created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
       <div className="hero-overlay bg-opacity-60" data-aos="zoom-in"></div>
       <div className="text-center hero-content text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold" data-aos="zoom-in">Hello there</h1>
-          <p className="mb-5" data-aos="zoom-in">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <h1 className="mb-5 text-5xl font-bold" data-aos="zoom-in">Building a Collaborative Platform with React.js for Frontend Excellence and Express, Node.js, and MongoDB for Backend Efficiency.</h1>
+          <p className="mb-5" data-aos="zoom-in">mbark on a journey to create a responsive and visually appealing task management platform that redefines collaboration and enhances task efficiency. Our goal is to provide an intuitive experience that transforms task management for individuals and teams</p>
           {user ? (
             <Link to={"/dashboard"}>
               <button className="text-white btn btn-outline btn-success">
